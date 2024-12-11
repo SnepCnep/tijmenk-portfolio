6c533f76f22f766c36af10ecaf25453016f75dc5
SnepCnep <93768063+SnepCnep@users.noreply.github.com>
2024-12-11 15:03:30 +0100

    refactor: simplify GET request handler by removing unnecessary context parameter

--- src/app/api/v1/[...slug]/route.ts
+++ src/app/api/v1/[...slug]/route.ts
@@ -1,11 +1,8 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
 
+export async function GET(request: NextRequest, { params }: { params: { slug: string[] } }) {
+  const { slug } = params;
+  const [username, ...rest] = slug;
 
-
-
-export async function GET(request: NextRequest, context: { params: { slug: string[] } }) {
-    const { slug } = context.params;
-    const [username, ...rest] = slug;
-    
-    return NextResponse.json({ username, rest });
+  return NextResponse.json({ username, rest });
 }
