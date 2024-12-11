9c9459c03edfc0606062d37669ae3c55c91cd8d5
SnepCnep <93768063+SnepCnep@users.noreply.github.com>
2024-12-11 15:05:41 +0100

    refactor: remove unused GET request handler for slug API endpoint

--- src/app/api/v1/[...slug]/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export async function GET(request: NextRequest, { params }: { params: { slug: string[] } }) {
-  const { slug } = params;
-  const [username, ...rest] = slug;
-
-  return NextResponse.json({ username, rest });
-}
