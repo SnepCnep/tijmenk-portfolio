d250bd964a74894997d27870ce783eda11226e6a
snepcnep <93768063+SnepCnep@users.noreply.github.com>
2024-12-17 22:47:39 +0100

    (fix): Navbar links.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -55,16 +55,16 @@ const Navbar = () => {
                     <NavigationMenuList className="flex gap-6">
                         {navbarItems.map((item) => (
                             <TooltipProvider key={item.title}>
-                                <Tooltip>
-                                    <TooltipTrigger>
-                                        <NavigationMenuItem className={navigationMenuTriggerStyle()}>
-                                            <Link href={item.href}>
+                                <Link href={item.href}>
+                                    <Tooltip>
+                                        <TooltipTrigger>
+                                            <NavigationMenuItem className={navigationMenuTriggerStyle()}>
                                                 {item.title}
-                                            </Link>
-                                        </NavigationMenuItem>
-                                    </TooltipTrigger>
-                                    <TooltipContent className="bg-gray-900">{item.description}</TooltipContent>
-                                </Tooltip>
+                                            </NavigationMenuItem>
+                                        </TooltipTrigger>
+                                        <TooltipContent className="bg-gray-900">{item.description}</TooltipContent>
+                                    </Tooltip>
+                                </Link>
                             </TooltipProvider>
                         ))}
                     </NavigationMenuList>
@@ -77,27 +77,27 @@ const Navbar = () => {
 export default Navbar;
 
 const ListItem = React.forwardRef<
-  React.ElementRef<"a">,
-  React.ComponentPropsWithoutRef<"a">
+    React.ElementRef<"a">,
+    React.ComponentPropsWithoutRef<"a">
 >(({ className, title, children, ...props }, ref) => {
-  return (
-    <li>
-      <NavigationMenuLink asChild>
-        <a
-          ref={ref}
-          className={cn(
-            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-            className
-          )}
-          {...props}
-        >
-          <div className="text-sm font-medium leading-none">{title}</div>
-          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-            {children}
-          </p>
-        </a>
-      </NavigationMenuLink>
-    </li>
-  );
+    return (
+        <li>
+            <NavigationMenuLink asChild>
+                <a
+                    ref={ref}
+                    className={cn(
+                        "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+                        className
+                    )}
+                    {...props}
+                >
+                    <div className="text-sm font-medium leading-none">{title}</div>
+                    <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                        {children}
+                    </p>
+                </a>
+            </NavigationMenuLink>
+        </li>
+    );
 });
 ListItem.displayName = "ListItem";
